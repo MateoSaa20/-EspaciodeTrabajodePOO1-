14e5734baeb3cdd67511a9b6b0d038a4420494c4
MateoSaa20 <mateosaavedra469@gmail.com>
2026-05-14 22:17:31 -0300

    corrección ejercicio 2

--- TrabajoPractico2/Ejercicio2/GestionEstudiantes.java
+++ TrabajoPractico2/Ejercicio2/GestionEstudiantes.java
@@ -83,7 +83,41 @@ public class GestionEstudiantes {
 
         return suma / estudiantes.size();
     }
+    public static class Estudiante {
 
+    private String nombre;
+    private int legajo;
+    private double promedio;
+
+    public Estudiante(String nombre, int legajo, double promedio) {
+        this.nombre = nombre;
+        this.legajo = legajo;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public boolean estaAprobado() {
+        return promedio >= 6.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante(nombre = " + nombre +
+                ", legajo = " + legajo +
+                ", promedio = " + promedio + ")";
+    }
+}
     public static void main(String[] args) {
 
         GestionEstudiantes gestion = new GestionEstudiantes();
@@ -151,4 +185,5 @@ public class GestionEstudiantes {
 
         System.out.println("Promedio general: " + gestion.calcularPromedioGeneral());
     }
+
 }
